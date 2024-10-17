ab2163ee8ff0823d64ab45e378751bca3125fd3a
Oleksandr Ilchenko <ilchenko.sasha@gmail.com>
2024-10-17 18:43:58 +0200

    add ChainExaminer

---
+++ sdk/chainExaminer/chainexaminer.js
@@ -0,0 +1,14 @@
+const evm = require("./evm/EVM");
+
+class ChainExaminer {
+    constructor(config) {
+      this.config = config;
+      this.evm = new evm(config);
+      
+    }
+  }
+  
+
+  
+  module.exports = ChainExaminer;
+  
